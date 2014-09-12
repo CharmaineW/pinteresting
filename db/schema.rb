869c3814a402f3b92c2239dc7aaf86bda9e0474e
Charmaine Wallace <charmainewallace@btinternet.com>
2014-09-12 16:30:03 +0100

    Add pins/users association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911151305) do
+ActiveRecord::Schema.define(version: 20130901081152) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20140911151305) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
